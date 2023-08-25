f578e695b12ccc156b9865c51ed60e9d02cee149
Sky Sayson <Skysayson74@gmail.com>
2023-08-25 20:07:14 +0800

    init commit

---
+++ Lecture-code/Data Types/dt-script.js
@@ -0,0 +1,18 @@
+
+// A value is primitive by default unless it is an object.
+
+/* 
+
+7 Primitive Data Types
+
+-- Most commonly used --
+
+Number: Default floating point numbers -> Used for decimals and integers : let age = 23;
+
+String: Sequence of characters -> Used for text : let firstName = "Jonaz"
+
+Boolean: Logical type, can only be true or false -> Used for making/taking decisions : let fullAge = true;
+
+*/
+
+  
---
+++ Lecture-code/Values and Variables/vav-script.js
@@ -0,0 +1,30 @@
+let js = "amazing";
+console.log(40 * 8 * 23 - 10);
+
+const firstName = "Michael" //Declaring a varible
+console.log(firstName);
+
+
+//naming conventions - firstNamePerson - first_name_person
+//Variable can contain numbers, letters, underscores or $
+//Can't be a reseverd keyword name, function etc...
+
+
+let jonaz = "Sky";
+console.log(jonaz);
+//non const variables naming convention - all lowercase
+
+const SAYSON = "last name";
+//const variables naming convention - all uppercase
+
+let Juan = "Firstname";
+//OOP naming convention - first letter uppercase
+
+
+let myFirstJob = "Student"; // - Descriptive
+let job1 = "Programmer";// - Non-descriptive
+
+//Variable names should be descriptive
+
+//What is a varible? Ans: A box with a label where we can store a value
+
