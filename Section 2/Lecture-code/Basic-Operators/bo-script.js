a291f947f110341b67eab081eda8edc61fbf75d8
Sky Sayson <Skysayson74@gmail.com>
2023-08-26 23:40:51 +0800

    Rearranged files

---
+++ Section 2/Lecture-code/Basic-Operators/bo-script.js
@@ -0,0 +1,27 @@
+const year = 2023;
+const ageSky = year - 2001;
+const ageAngel = year - 1964;
+
+console.log(ageSky * 2, ageAngel / 2);
+//You're able to display and perform multiple operations on one log
+
+const firstName = "Jonaz Juan";
+const lastName = "Sayson";
+
+console.log(firstName + " " + lastName);
+//Concatenation
+
+//typeof() operator tells us what type a value is
+
+
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--; 
+console.log(x);
+// += operator means x = x + 10
+// x= operator means x = x * 10
+// x++ operator means x = x + 1
+// x-- operator means x = x - 1
+
---
+++ Section 2/Lecture-code/let-const-var/lcv-script.js
@@ -0,0 +1,29 @@
+let age = 30;
+
+age = 20;
+
+console.log(age);
+
+//Reassigning or mutating a variable
+
+const name = "Sky";
+
+console.log(name);
+
+//const variable cannot be reassigned/immutable
+//const job;
+//Const has to be initialised
+
+//Good practice is to always use const unless you're sure that a varible has to change
+//Changing variables has the potential to create bugs
+
+var sky = 12;
+console.log(sky);
+
+//Var is legacy (before ES6), works the same way as let. !NEVER USE VAR! 
+
+michael = 200;
+
+console.log(michael);
+
+//Declaration is not locally declared, it becomes globally declared
