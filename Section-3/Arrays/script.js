a7037f027ac4a29b7b31c97e29b77c4ac9cc99da
Sky Sayson <Skysayson74@gmail.com>
2023-09-09 14:53:23 +0800

    Finished array operations

--- Section-3/Arrays/script.js
+++ Section-3/Arrays/script.js
@@ -15,5 +15,34 @@ const years = new Array(2001,2021,2009);
 //console.log(friends);
 
 //In javascript an array can hold multiple types even other arrays
-const Sky = [`Jonaz Juan`,`Sayson`, friends];
-console.log(Sky + " " +`The length of the array is ${Sky.length}`);
+const Sky = [`Jonaz Juan`,`Sayson`];
+// console.log(Sky + " " +`The length of the array is ${Sky.length}`);
+
+//Pushes into the end of the array
+Sky.push('Angel');
+console.log(Sky);
+
+//Puts element in to the beginning of the array
+Sky.unshift('Lawr');
+console.log(Sky);
+
+//Remove and returns last element of the array;
+const popped = Sky.pop();
+console.log(popped);
+
+//Remove and return first element of the array
+Sky.shift();
+console.log(Sky);
+
+console.log(Sky.indexOf('Sayson'));
+
+
+//ES6 method of indexOf, returns true else false if element is in the array
+//No type coercion
+console.log(Sky.includes("Sayson"));
+
+const newNum = JSON.parse("23");
+Sky.push(newNum);
+console.log(Sky);
+
+console.log(Sky.includes(23));
